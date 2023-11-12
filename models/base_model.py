78d8c3e2e31453eaa2eb092bd7288f222e59ba8d
ahmed-mekky <27medsh3pan@gmail.com>
2023-11-12 08:01:26 +0200

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,15 @@
 import uuid
 import datetime
 """
-This module contains the classes for a simple database. It is used to store data in memory and retrieve it later.
+This module contains the classes for a simple database.
+It is used to store data in memory and retrieve it later.
 """
+
+
 class BaseModel:
     """
-    This is the base model for all other models. It provides common functionality to all models.
+    This is the base model for all other models.
+    It provides common functionality to all models.
     """
     def __init__(self, *args, **kwargs):
         """Constructor for the base model."""
@@ -19,7 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-    
+
     def __str__(self):
         """Returns a string representation of this object."""
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
@@ -29,7 +33,7 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Converts this object into a dictionary that can be serialized as JSON."""
+        """Converts this object into dictionary that can serialized as JSON."""
         data = {}
         for key, value in vars(self).items():
             if not callable(key) and not key.startswith("_"):
